8646d27f29b49e62d46305170c163f454ba5db5c
zbf <hgkdzbf6@qq.com>
2019-05-29 17:19:35 +0800

    test rpn

--- demo/test.py
+++ demo/test.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 import matplotlib.pylab as pylab
 import numpy as np
 import requests
+import torch
+import torchvision
+import cv2
 
 # config_file = "../configs/caffe2/e2e_mask_rcnn_R_50_FPN_1x_caffe2.yaml"
 config_file = "../configs/rpn_X_101_32x8d_FPN_1x.yaml"
@@ -20,6 +23,19 @@ def load(url):
     image = np.array(pil_image)[:, :, [2, 1, 0]]
     return image
 
+def draw(img, bbox):
+    bbox_num = bbox.shape[0]
+    for item in range(bbox_num):
+        a = (bbox[item,0],bbox[item,1])
+        b = (bbox[item,2],bbox[item,1])
+        c = (bbox[item,2],bbox[item,3])
+        d = (bbox[item,0],bbox[item,3])
+        cv2.line(img,a,b,(0,255,0),3)
+        cv2.line(img,b,c,(0,255,0),3)
+        cv2.line(img,c,d,(0,255,0),3)
+        cv2.line(img,d,a,(0,255,0),3)
+    cv2.imwrite('2.png', img)
+    
 
 # update the config options with the config file
 cfg.merge_from_file(config_file)
@@ -34,7 +50,10 @@ coco_demo = COCODemo(
 # load image and then run prediction
 image = load('./1.jpg')
 predictions = coco_demo.compute_prediction(image)
-#plt.imshow(predictions[:,:,[2,1,0]])
-#plt.axis('off')
-#plt.savefig('2.png')
-print(predictions.bbox)
+print(predictions.bbox.numpy())
+draw(image,predictions)
+# plt.imshow(predictions[:,:,[2,1,0]])
+# plt.axis('off')
+# plt.savefig('2.png')
+
+
